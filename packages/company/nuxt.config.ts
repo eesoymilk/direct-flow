03b5e3c26c84653845ab84f809f5b02748cf144f
eesoymilk@gmail.com <eesoymilk@gmail.com>
2025-09-09 11:06:40 -0700

    chore: remove unused company assets and configurations
    - Delete pnpm-lock.yaml files for both base and company packages to clean up unused lock files.
    - Remove businessScopes.csv and businessScopes.json files as they are no longer needed.
    - Eliminate csv_to_json.py script which was used for converting CSV to JSON.
    - Clear out main.css file to remove obsolete styles.
    
    This cleanup helps streamline the project structure and reduces unnecessary files.

--- packages/company/nuxt.config.ts
+++ packages/company/nuxt.config.ts
@@ -1,39 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["../base"],
-
-  compatibilityDate: "2025-07-25",
-  devtools: { enabled: true },
-
-  future: {
-    compatibilityVersion: 4,
-  },
-
-  nitro: {
-    experimental: {
-      database: true,
-    },
-  },
-
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/ui",
-    "@nuxt/content",
-    "@nuxt/test-utils/module",
-    "@pinia/nuxt",
-    "nuxt-auth-utils",
-    "@vueuse/nuxt",
-  ],
-
-  css: ["~/assets/css/main.css"],
-
-  ui: {
-    colorMode: false,
-  },
-
   imports: {
     dirs: ["~/shared/**"],
   },
